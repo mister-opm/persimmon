b525613ce3d0dda4a05dfbb9efd46d91fa9f747c
jkwizzle <justinkwon123@gmail.com>
2022-06-25 14:15:21 -0400

    'progress'

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,10 @@
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-    // https://nextjs.org/docs/messages/no-page-custom-font
     try {
       ctx.renderPage = () =>
         originalRenderPage({
@@ -14,6 +13,7 @@ export default class MyDocument extends Document {
         });
 
       const initialProps = await Document.getInitialProps(ctx);
+
       return {
         ...initialProps,
         styles: (
@@ -26,5 +26,23 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,32 +9,28 @@ const Home: NextPage = () => {
         <title>Persimmon</title>
         <meta name="description" content="Persimmon" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <MiddleContainer>
-        <WelcomeSign>Welcome</WelcomeSign>
+        <WelcomeSign>Welcome to you</WelcomeSign>
+        <SubTitle>We&apos;ve been waiting for you.</SubTitle>
       </MiddleContainer>
     </MainContainer>
   );
 };
 
 const MainContainer = styled.div`
+  display: flex;
   height: 100vh;
   width: 100vw;
   background-color: rgba(245, 69, 245, 50%);
   font-family: "DM Sans", sans-serif;
+  justify-content: center;
 `;
 
 const MiddleContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  text-align: center;
+  align-self: center;
 `;
 
 const WelcomeSign = styled.h1`
@@ -43,6 +39,15 @@ const WelcomeSign = styled.h1`
   justify-content: center;
   text-align: center;
   font-family: "DM Sans", sans-serif;
+  font-size: 48px;
+`;
+
+const SubTitle = styled.span`
+  font-size: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
 `;
 
 export default Home;
